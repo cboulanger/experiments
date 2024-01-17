dc1f90d72a5563808f2381c474504b66c2b06ff8
Christian Boulanger <info@bibliograph.org>
2024-01-17 22:08:40 +0100

    Updates

--- langchain-experiments/lib/hf_llama2_chat_gptq.py
+++ langchain-experiments/lib/hf_llama2_chat_gptq.py
@@ -1,5 +1,4 @@
 import requests
-import os
 from dotenv import load_dotenv
 from json import JSONDecodeError
 import os
@@ -7,25 +6,24 @@ import os
 load_dotenv()
 
 API_KEY = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-API_URL = "https://gp8iviqlqee101a0.us-east-1.aws.endpoints.huggingface.cloud"
 headers = {
     "Accept" : "application/json",
     "Authorization": f"Bearer {API_KEY}",
     "Content-Type": "application/json"
 }
 
-def query(template, model_params = None, **params):
+def query(url, template, model_params = None, **params):
     if model_params  is None:
         model_params = {
             "temperature": 0.1,
-            "max_new_tokens": 1000
+            "max_new_tokens": 2000
         }
-    prompt = template.format_map(params)
+    inputs = template.format_map(**params)
     payload = {
-        "inputs": f"<s>[INST] <<SYS>>You are a helpful assistant. No comments or explanation, just answer the question.<</SYS>>{prompt}[/INST]",
+        "inputs": inputs,
         "parameters": model_params
     }
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
     try:
         return response.json()[0].get("generated_text")
